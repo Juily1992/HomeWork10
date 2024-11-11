5996999edb75bf78e5a659603abbe27ab55a519a
Juily1992 <juilliet1992@gmail.com>
2024-11-11 16:13:08 +0300

    Проверка

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,23 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Task 1
+        System.out.println("Задание 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = firstName + ' ' + middleName + ' ' + lastName;
+        System.out.println("Ф.И.О. сотредника - " + fullName);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+    // Task 2
+        System.out.println(" ");
+        System.out.println("Задание 2");
+        System.out.println("Данные сотрудника для заполнения отчета - " + fullName.toUpperCase());
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        // Task 3
+        System.out.println(" ");
+        System.out.println("Задание 3");
+        String fullNameRussian = "Иванов Семён Иванович";
+        System.out.println("Данные сотрудника для заполнения отчета - " + fullNameRussian.replace('ё', 'е'));
     }
 }
